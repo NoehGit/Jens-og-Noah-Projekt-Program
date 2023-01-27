fdf3f8eb2366e1c94567f77f90a5b13e8b175ca4
Noeh <117693077+NoehGit@users.noreply.github.com>
2023-01-27 20:50:28 +0100

    Add files via upload

---
+++ Jens og Noah Projekt/Jens og Noah Projekt/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Jens_og_Noah_Projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.showBtn = new System.Windows.Forms.Button();
+            this.outputTextbox = new System.Windows.Forms.TextBox();
+            this.inputTextbox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.goBtn = new System.Windows.Forms.Button();
+            this.outputSelectBtn = new System.Windows.Forms.Button();
+            this.inputSelectBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.button10);
+            this.tabPage1.Controls.Add(this.button9);
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "TICTACTOE";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(230, 178);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(77, 38);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "RESET";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(160, 153);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(64, 63);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(90, 153);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(64, 63);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(20, 153);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(64, 63);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(160, 84);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(64, 63);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(90, 84);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(64, 63);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(20, 84);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(64, 63);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(160, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 63);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 63);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 63);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.showBtn);
+            this.tabPage2.Controls.Add(this.outputTextbox);
+            this.tabPage2.Controls.Add(this.inputTextbox);
+            this.tabPage2.Controls.Add(this.progressBar1);
+            this.tabPage2.Controls.Add(this.goBtn);
+            this.tabPage2.Controls.Add(this.outputSelectBtn);
+            this.tabPage2.Controls.Add(this.inputSelectBtn);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "IMG TIL ASCII";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // showBtn
+            // 
+            this.showBtn.Enabled = false;
+            this.showBtn.Location = new System.Drawing.Point(171, 95);
+            this.showBtn.Name = "showBtn";
+            this.showBtn.Size = new System.Drawing.Size(100, 23);
+            this.showBtn.TabIndex = 6;
+            this.showBtn.Text = "VIS BILLEDE";
+            this.showBtn.UseVisualStyleBackColor = true;
+            this.showBtn.Click += new System.EventHandler(this.showBtn_Click);
+            // 
+            // outputTextbox
+            // 
+            this.outputTextbox.Location = new System.Drawing.Point(171, 47);
+            this.outputTextbox.Name = "outputTextbox";
+            this.outputTextbox.Size = new System.Drawing.Size(100, 20);
+            this.outputTextbox.TabIndex = 5;
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.Location = new System.Drawing.Point(171, 6);
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.Size = new System.Drawing.Size(100, 20);
+            this.inputTextbox.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 158);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // goBtn
+            // 
+            this.goBtn.Enabled = false;
+            this.goBtn.Location = new System.Drawing.Point(11, 95);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(100, 23);
+            this.goBtn.TabIndex = 2;
+            this.goBtn.Text = "GO !";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // outputSelectBtn
+            // 
+            this.outputSelectBtn.Location = new System.Drawing.Point(11, 45);
+            this.outputSelectBtn.Name = "outputSelectBtn";
+            this.outputSelectBtn.Size = new System.Drawing.Size(100, 23);
+            this.outputSelectBtn.TabIndex = 1;
+            this.outputSelectBtn.Text = "Vælg Destination";
+            this.outputSelectBtn.UseVisualStyleBackColor = true;
+            this.outputSelectBtn.Click += new System.EventHandler(this.outputSelectBtn_Click);
+            // 
+            // inputSelectBtn
+            // 
+            this.inputSelectBtn.Location = new System.Drawing.Point(11, 3);
+            this.inputSelectBtn.Name = "inputSelectBtn";
+            this.inputSelectBtn.Size = new System.Drawing.Size(100, 23);
+            this.inputSelectBtn.TabIndex = 0;
+            this.inputSelectBtn.Text = "Vælg Fil";
+            this.inputSelectBtn.UseVisualStyleBackColor = true;
+            this.inputSelectBtn.Click += new System.EventHandler(this.inputSelectBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Progress:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(316, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(370, 296);
+            this.label2.TabIndex = 8;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(342, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(401, 201);
+            this.label3.TabIndex = 10;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button outputSelectBtn;
+        private System.Windows.Forms.Button inputSelectBtn;
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.TextBox outputTextbox;
+        private System.Windows.Forms.TextBox inputTextbox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button showBtn;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Jens og Noah Projekt/Jens og Noah Projekt/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jens_og_Noah_Projekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        // ASCII APP :
+        public static double Brightness(Color c)
+        {
+            return (0.2126 * c.R + 0.7152 * c.G + 0.0722 * c.B);
+        }
+
+        private void inputSelectBtn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "c:\\";
+
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string inputFilename = openFileDialog1.FileName;
+                inputTextbox.Text = inputFilename;
+
+                progressBar1.Value = 0;
+
+                if (inputTextbox.Text != "" && outputTextbox.Text != "")
+                {
+                    goBtn.Enabled= true;
+                }
+
+                try
+                {
+                    var image = new Bitmap(inputFilename);
+                    int pixelAmount = image.Height * image.Width;
+                    progressBar1.Maximum = pixelAmount;
+                }
+                catch (ArgumentException)
+                {
+                    MessageBox.Show("Den valgte fil er ikke et billede, prøv igen");
+                    inputTextbox.Clear();
+                    goBtn.Enabled = false;
+                    return;
+                }
+            }
+        }
+
+        private void outputSelectBtn_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.InitialDirectory = "c:\\";
+            DialogResult result = openFileDialog2.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string outputFilename = openFileDialog2.FileName;
+                outputTextbox.Text = outputFilename;
+                progressBar1.Value=0;
+            }
+            if (inputTextbox.Text != "" && outputTextbox.Text != "")
+            {
+                goBtn.Enabled= true;
+            }
+        }
+
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+            string imageLocation = inputTextbox.Text;
+            string outputLocation = outputTextbox.Text;
+            string AsciiColor = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,^'. ";
+
+            var img = new Bitmap(imageLocation);
+
+            using (var wrtr = new StreamWriter(outputLocation))
+            {
+                for (var y = 0; y < img.Height; y++)
+                {
+                    for (var x = 0; x < img.Width; x++)
+                    {
+                        var color = img.GetPixel(x, y);
+                        var brightness = Brightness(color);
+                        var idx = brightness / 255 * (AsciiColor.Length - 1);
+                        var pxl = AsciiColor[AsciiColor.Length - (int)Math.Round(idx) - 1];
+                        wrtr.Write(pxl);
+                        progressBar1.Step = 1;
+                        progressBar1.PerformStep();
+                    }
+                    wrtr.WriteLine();
+                }
+            }
+            showBtn.Enabled= true;
+        }
+
+        private void showBtn_Click(object sender, EventArgs e)
+        {
+            string path = outputTextbox.Text;
+            System.Diagnostics.Process.Start(path);
+        }
+
+        // END OF ASCII APP
+
+        // TIC TAC TOE APP :
+
+        String[] gameBoard = new string[9];
+        int currentTurn = 0;
+
+        public String returnSymbol(int turn)
+        {
+            if (turn % 2 == 0)
+            {
+                return "O";
+            }
+            else
+            {
+                return "X";
+            }
+        }
+
+        public void checkForWinner()
+        {
+            for (int i=0; i<8; i++)
+            {
+                String combination = "";
+
+                switch (i)
+                {
+                    case 0:
+                        combination= gameBoard[0] + gameBoard[4] + gameBoard[8];
+                        break;
+                    case 1:
+                        combination = gameBoard[2] + gameBoard[4] + gameBoard[6];
+                        break;
+                    case 2:
+                        combination = gameBoard[0] + gameBoard[1] + gameBoard[2];
+                        break;
+                    case 3:
+                        combination = gameBoard[3] + gameBoard[4] + gameBoard[5];
+                        break;
+                    case 4:
+                        combination = gameBoard[6] + gameBoard[7] + gameBoard[8];
+                        break;
+                    case 5:
+                        combination = gameBoard[0] + gameBoard[3] + gameBoard[6];
+                        break;
+                    case 6:
+                        combination = gameBoard[2] + gameBoard[4] + gameBoard[7];
+                        break;
+                    case 7:
+                        combination = gameBoard[3] + gameBoard[5] + gameBoard[8];
+                        break;
+                }
+
+                if (combination.Equals("OOO"))
+                {
+                    reset();
+                    MessageBox.Show("O har vundet!", "Vinderen er fundet", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        public void reset()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+
+            gameBoard = new string[9];
+            currentTurn = 0;
+        }
+
+        public void checkDraw()
+        {
+            int counter = 0;
+            for (int i=0; i<gameBoard.Length; i++)
+            {
+                if (gameBoard[i] != null)
+                {
+                    counter++;
+                }
+                if (counter== 9)
+                {
+                    reset();
+                    MessageBox.Show("Spillet er uafgjort", "Vinderen er ikke fundet?!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                checkDraw();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[0] = returnSymbol(currentTurn);
+            button1.Text = gameBoard[0];
+            checkForWinner();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[1] = returnSymbol(currentTurn);
+            button2.Text = gameBoard[1];
+            checkForWinner();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[2] = returnSymbol(currentTurn);
+            button3.Text = gameBoard[2];
+            checkForWinner();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[3] = returnSymbol(currentTurn);
+            button4.Text = gameBoard[3];
+            checkForWinner();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[4] = returnSymbol(currentTurn);
+            button5.Text = gameBoard[4];
+            checkForWinner();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[5] = returnSymbol(currentTurn);
+            button6.Text = gameBoard[5];
+            checkForWinner();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[6] = returnSymbol(currentTurn);
+            button7.Text = gameBoard[6];
+            checkForWinner();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[7] = returnSymbol(currentTurn);
+            button8.Text = gameBoard[7];
+            checkForWinner();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[8] = returnSymbol(currentTurn);
+            button9.Text = gameBoard[8];
+            checkForWinner();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+    }
+}
